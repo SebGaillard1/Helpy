9b7ebf91d6bb2d7fd72e9e92b19458d76dde7f08
Sebastien Gaillard <gaillard.sebastien@outlook.fr>
2022-02-27 15:31:43 +0100

    Config change

--- Helpy/AppDelegate.swift
+++ Helpy/AppDelegate.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 import Firebase
-import GooglePlaces
+//import GooglePlaces
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
-        GMSPlacesClient.provideAPIKey(ApiKeys.googlePlacesApiKey)
+       // GMSPlacesClient.provideAPIKey(ApiKeys.googlePlacesApiKey)
         return true
     }
 
