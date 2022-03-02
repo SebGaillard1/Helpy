59b76c2f29a632f489d392434f9767d407cf3020
Sebastien Gaillard <gaillard.sebastien@outlook.fr>
2022-03-02 21:23:39 +0100

    Added launch animation to hide authentication check

--- Helpy/Controllers/LogProcess/WelcomeViewController.swift
+++ Helpy/Controllers/LogProcess/WelcomeViewController.swift
@@ -7,8 +7,12 @@
 
 import UIKit
 import Firebase
+import AVFoundation
 
 class WelcomeViewController: UIViewController {
+    //MARK: - Outlets
+    @IBOutlet weak var videoUIImageView: UIImageView!
+    
     //MARK: - Properties
     var handle: AuthStateDidChangeListenerHandle?
     
@@ -19,6 +23,8 @@ class WelcomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        playVideo()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -27,18 +33,23 @@ class WelcomeViewController: UIViewController {
           // 2
             guard let user = user else {
                 self.navigationController?.popToRootViewController(animated: true)
+                self.videoUIImageView.isHidden = true
                 return
             }
             
             Constants.FirebaseHelper.clientRef.whereField("uid", isEqualTo: user.uid).getDocuments { querySnapshot, error in
                 if querySnapshot?.isEmpty == false && error == nil {
+                    UIView.setAnimationsEnabled(false)
                     self.performSegue(withIdentifier: self.segueIdWelcomeToClientHome, sender: nil)
+                    self.videoUIImageView.isHidden = true
                 }
             }
             
             Constants.FirebaseHelper.proRef.whereField("uid", isEqualTo: user.uid).getDocuments { querySnapshot, error in
                 if querySnapshot?.isEmpty == false && error == nil {
+                    UIView.setAnimationsEnabled(false)
                     self.performSegue(withIdentifier: self.segueIdWelcomeToProHome, sender: nil)
+                    self.videoUIImageView.isHidden = true
                 }
             }
         }
@@ -47,7 +58,20 @@ class WelcomeViewController: UIViewController {
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
         
+        UIView.setAnimationsEnabled(true)
+        
         guard let handle = handle else { return }
         Auth.auth().removeStateDidChangeListener(handle)
+    }
+    
+    private func playVideo() {
+        guard let path = Bundle.main.path(forResource: "launchVideo", ofType: "mp4") else {
+            print("Pas trouvé")
+            return }
+        let player = AVPlayer(url: URL(fileURLWithPath: path))
+        let playerLayer = AVPlayerLayer(player: player)
+        playerLayer.frame = videoUIImageView.bounds
+        videoUIImageView.layer.addSublayer(playerLayer)
+        player.play()
     }
 }
