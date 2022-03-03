d19350e5ea2f12f839e466090fabc7042d54cea3
Sebastien Gaillard <gaillard.sebastien@outlook.fr>
2022-03-03 01:00:09 +0100

    Added map to indicate post location approximately in detail post vc

---
+++ Helpy/ApiKeys.swift
@@ -0,0 +1,12 @@
+//
+//  ApiKeys.swift
+//  Helpy
+//
+//  Created by Sebastien Gaillard on 13/02/2022.
+//
+
+import Foundation
+
+struct ApiKeys {
+    static let googlePlacesApiKey = "AIzaSyDxYSfTlAkaPoZC_6PIBSS4qReO8FRPS74"
+}
--- Helpy/Controllers/Client/PostDetailsViewController.swift
+++ Helpy/Controllers/Client/PostDetailsViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import MapKit
 
 class PostDetailsViewController: UIViewController {
     //MARK: - Outlets
@@ -17,14 +18,19 @@ class PostDetailsViewController: UIViewController {
     @IBOutlet weak var postCityAndPostalCodeLabel: UILabel!
     @IBOutlet weak var postPostedByLabel: UILabel!
     
+    @IBOutlet weak var postMKMapView: MKMapView!
     //MARK: - Properties
     var post: Post!
     
     //MARK: - View life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         setupUi()
+        
+        postMKMapView.delegate = self
+        let location = CLLocation(latitude: post.latitude, longitude: post.longitude)
+        centerMapOnLocation(location, mapView: postMKMapView)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -33,7 +39,7 @@ class PostDetailsViewController: UIViewController {
         navigationController?.navigationBar.isHidden = false
         navigationController?.interactivePopGestureRecognizer?.isEnabled = true
     }
-
+    
     private func setupUi() {
         postImageView.image = post.image
         postTitleLabel.text = post.title
@@ -45,4 +51,35 @@ class PostDetailsViewController: UIViewController {
             postDateLabel.text = "\(date)"
         }
     }
+    
+    // Radius is measured in meters
+    private func centerMapOnLocation(_ location: CLLocation, mapView: MKMapView) {
+        mapView.isUserInteractionEnabled = false
+        let regionRadius: CLLocationDistance = 2000
+        let coordinateRegion = MKCoordinateRegion(center: location.coordinate,
+                                                  latitudinalMeters: regionRadius * 2.0, longitudinalMeters: regionRadius * 2.0)
+        mapView.setRegion(coordinateRegion, animated: true)
+        
+        let location2d = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        showCircle(coordinate: location2d, radius: 1000)
+    }
+    
+    // Radius is measured in meters
+    func showCircle(coordinate: CLLocationCoordinate2D, radius: CLLocationDistance) {
+        let circle = MKCircle(center: coordinate, radius: radius)
+        postMKMapView.addOverlay(circle)
+    }
 }
+
+//MARK: - Extension
+extension PostDetailsViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        let circleOverlay = overlay
+        let circleRenderer = MKCircleRenderer(overlay: circleOverlay)
+        circleRenderer.fillColor = .systemBlue
+        circleRenderer.alpha = 0.40
+        
+        return circleRenderer
+    }
+}
+
